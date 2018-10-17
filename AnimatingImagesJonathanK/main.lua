6c09ca4b4f1445a0045a0fce9fe91dfd9891e79d
Jonathan <jonathan.kene@ocsbstudent.ca>
2018-10-16 20:41:24 -0400

    added animated images

--- AnimatingImagesJonathanK/main.lua
+++ AnimatingImagesJonathanK/main.lua
@@ -1,7 +1,74 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
+-- Title: Animating Images
+-- Name: Jonathan Kene
+-- Course: ISC2O/3C
+-- This program displays animating images moving different directions.
 -----------------------------------------------------------------------------------------
 
--- Your code here
+
+--global variables
+scrollSpeed = 6
+
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- set background image with width and height
+local backgroundImage = display.newImageRect("Images/space.jpg", 2048, 1600)
+
+-- set character image with width and height
+local kirby = display.newImage("Images/kirby.png", 200, 200)
+kirby.x = 100
+kirby.x = 125
+
+-- set character image with width and height
+local kirbytwo = display.newImage("Images/kirbytwo.png", 100, 200)
+
+-- set character image with width and height
+local kirbythewicked = display.newImage("Images/kirbythewicked.jpg", 600, 600)
+
+-- set the image to be transparent 
+kirbytwo.alpha = 0
+
+-- flip kirbytwo horizontally
+kirbytwo.xScale = 1
+
+-- rotate kirbytwo 45 degrees
+kirbytwo.rotation = 180
+print( kirby.rotation )  -- Prints 45 in the terminal
+
+-- rotate kirby 45 degrees
+kirby.rotation = 45
+print( kirby.rotation )  -- Prints 45 in the terminal
+
+local textObject
+--displays text on the screen at position x = 500 and y = 700 with
+-- a deafult font style and font size of 70
+textObject = display.newText( "I'll get you two next time!!", 600, 725, nil, 50 )
+
+-- Function: MoveKirby
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description: This function adds the scroll speed to the x-value of the kirby
+local function MoveKirby(event)
+	-- add the scroll speed to the x-value of the kirby
+	kirby.x = kirby.x + scrollSpeed
+end
+
+-- MoveShip will be called over and over again
+Runtime:addEventListener("enterFrame", MoveKirby)
+
+-- Function: MoveKirbyTwo
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description: This function adds the scroll speed to the x-value of the kirbytwo
+local function MoveKirbyTwo(event)
+	-- add the scroll speed to the x-value of the kirbytwo
+	kirbytwo.x = kirbytwo.x + scrollSpeed
+	-- change the transparency of the kirbytwo every time it moves so that it fades out
+	 kirbytwo.alpha = kirbytwo.alpha + 0.01
+end
+
+transition.fadeOut( kirby, { time=2000 } )
+
+-- MoveShip will be called over and over again
+Runtime:addEventListener("enterFrame", MoveKirbyTwo)
--- DisplayingShapesJonathanK/main.lua
+++ DisplayingShapesJonathanK/main.lua
@@ -4,16 +4,45 @@
 -- Course: ICS2O /3C
 -- Title: Display Shapes 
 -- This program displays different shapes/polygons with its name underneath in on iPad simulator
------------------------------------------------------------------------------------------
 
--- create my local variables
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- set the backround color
+local background = display.setDefault( "background", 40/255, 40/255, 238/255 )
 
 local verticesTriangle
 local verticesTriangle = {-100, -75, 0, -300, 100, -75}
-local triangleX = 500
-local triangleY = 500
+local triangleX = 200
+local triangleY = 300
 local myTriangle
 local myTriangle = display.newPolygon (triangleX, triangleY, verticesTriangle)
+ 
+local textObject
+-- displays text on the screen at position x = 500 and y = 500 with
+-- a deafult font style and font size of 70
+textObject = display.newText( "Triangle", 200, 500, nil, 70 )
 
+local verticesDiamond
+local verticesDiamond = {-100, -75, 0, -250, 100, -75, -2, 100}
+local diamondX = 800
+local diamondY = 220
+local myDiamond
+local myDiamond = display.newPolygon (diamondX, diamondY, verticesDiamond)
 
+local textObject
+-- displays text on the screen at position x = 800 and y = 450 with
+-- a deafult font style and font size of 70
+textObject = display.newText( "Diamond", 800, 450, nil, 70 )
+
+local verticesDiamond
+local verticesDiamond = {-80, -75, 10, -150, 90, -75, 55, 70, -80, -30, -20, 10, -60, -10}
+local diamondX = 500
+local diamondY = 550
+local myDiamond
+local myDiamond = display.newPolygon (diamondX, diamondY, verticesDiamond)
+
+--displays text on the screen at position x = 500 and y = 700 with
+-- a deafult font style and font size of 70
+textObject = display.newText( "Pentagon", 500, 700, nil, 70 )
 
