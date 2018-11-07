129d0e066740ae68b75c4c0659fd330a40927db3
Jonathan Kene <Q3erty7>
2018-11-07 11:10:02 -0500

    added Company Logo

--- CompanyLogoJonathanK/main.lua
+++ CompanyLogoJonathanK/main.lua
@@ -6,14 +6,14 @@
 -----------------------------------------------------------------------------------------
 
 -- global variables
-scrollSpeed = 6
+scrollSpeed = 3
 
 -- hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
-local shield = display.newImage( "Images/shield.png", 200, 200)
+local shield = display.newImageRect( "Images/shield.png", 300, 300)
 shield.x = 200
-shield.y = 300
+shield.y = 400
 
 -- set the image to be transparent 
 shield.alpha = 0
@@ -21,16 +21,19 @@ shield.alpha = 0
 local function MoveShield(event)
 	-- add the scroll speed to the x-value of the shield
 	shield.x = shield.x + scrollSpeed
+
 	-- change the transparency of the shield every time it moves so that it fades out
 	 shield.alpha = shield.alpha + 0.01
+
+
+
 end
 
- transition.fadeOut( kirby, { time=2000 } )
-	 
- transition.scaleTo( shield, { xScale = 2, yScale = 2, time = 1000 } )
 
- transition.cancel( 500, 300, MoveShip)
+transition.scaleTo( shield, { xScale = 1, yScale = 1, time = 1000 } )
 
+ transition.fadeOut( shield, { time= 100 } )
+ 
 -- MoveShip will be called over and over again
 Runtime:addEventListener("enterFrame", MoveShield)
 
