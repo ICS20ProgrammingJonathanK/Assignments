d39fa4c3611f2e9c70bcdea569c3e361f0ad0284
Jonathan Kene <Q3erty7>
2018-11-12 13:32:10 -0500

    Added Company Logo

--- CompanyLogoJonathanK/main.lua
+++ CompanyLogoJonathanK/main.lua
@@ -6,34 +6,40 @@
 -----------------------------------------------------------------------------------------
 
 -- global variables
-scrollSpeed = 3
+scrollSpeed = 100
 
 -- hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
-local shield = display.newImageRect( "Images/shield.png", 300, 300)
-shield.x = 200
-shield.y = 400
+local sword = display.newImageRect( "Images/sword.png", 500, 500)
+sword.x = 50
+sword.y = 400
 
--- set the image to be transparent 
-shield.alpha = 0
-
-local function MoveShield(event)
-	-- add the scroll speed to the x-value of the shield
-	shield.x = shield.x + scrollSpeed
-
-	-- change the transparency of the shield every time it moves so that it fades out
-	 shield.alpha = shield.alpha + 0.01
+-- displays text on the screen at position x = 400 and y = 5 with
+-- a deafult font style and font size of 30
+textObject = display.newText( "Dark Exalibur", 500, 650, nil, 100)
 
 
-
+local function GrowSword(event)
+	-- add the scroll speed to the x-value of the sword
+	sword.x = sword.x + scrollSpeed
+     -- make the sword grow
+	 transition.scaleTo( sword, { xScale = 2, yScale = 2, time = 1000 } )
+	 transition.fadeOut( sword, { time = 2000 } )
 end
 
 
-transition.scaleTo( shield, { xScale = 1, yScale = 1, time = 1000 } )
-
- transition.fadeOut( shield, { time= 100 } )
- 
+-- Function: MoveSword 
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description: This function adds the scroll speed to the x-value of the sword
+local function MoveSword(event)
+	-- add the scroll speed to the x-value of the ship
+	sword.x = sword.x + scrollSpeed
+	transition.fadeOut( textObject, { time = 2000 } )
+end
 -- MoveShip will be called over and over again
-Runtime:addEventListener("enterFrame", MoveShield)
+Runtime:addEventListener("enterFrame", MoveSword)
+Runtime:addEventListener("enterFrame", GrowSword)
+
 
